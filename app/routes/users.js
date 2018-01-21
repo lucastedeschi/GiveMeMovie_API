de9f3da621bfcc9885f0a074ff657ed04cd01edd
lrt.tedeschi@gmail.com <lrt.tedeschi@gmail.com>
2018-01-21 21:12:53 -0200

    Refatoração: Isolamento da conexão com o bd

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,17 +1,12 @@
+var conn = require('../infra/dbConnection')();
+
 module.exports = function(app) {
     app.get('/users', function(res, res){
-        var mysql = require('mysql');
-        var connection = mysql.createConnection({
-            host: "localhost",
-            user: "root",
-            password: '123456',
-            database: 'GiveMeMovie'
-        });
-
-        connection.query('select * from users', function(err, result) {
+        
+        conn.query('select * from users', function(err, result) {
             res.render('users/list', {list: result});
         });
  
-        connection.end();
+        conn.end();
     });
 }

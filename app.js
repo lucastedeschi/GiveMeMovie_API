0186bc4a588497a86664528c7428fad9a016a865
Lucas Rezende Tedeschi <lucastedeschi@outlook.com>
2018-02-25 20:27:21 -0300

    Fix² - Configurando Heroku

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 var app = require('./config/express')();
 
-app.listen(3000, 'localhost', function(){
-    console.log("Server is running!");
-});
-
+var port_number = server.listen(process.env.PORT || 3000);
+app.listen(port_number);
 
